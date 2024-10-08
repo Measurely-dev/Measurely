897fcdffbc6b09b7d7092aa652e42f16859fad8f
zakary.afz <zakaryfofana@gmail.com>
2024-10-08 11:49:43 -0400

    frontend: settings

--- frontend/src/components/dashboard/layout/navbar/navbar.tsx
+++ frontend/src/components/dashboard/layout/navbar/navbar.tsx
@@ -1,12 +1,6 @@
 import LogoSvg from "@/components/global/logoSvg";
 import Link from "next/link";
-import {
-  BarChart,
-  Box,
-  CreditCard,
-  Settings,
-  Users,
-} from "react-feather";
+import { BarChart, Box, CreditCard, Settings, Users } from "react-feather";
 import DashboardNavItem from "./navItem";
 import {
   Tooltip,
@@ -18,6 +12,7 @@ import { usePathname } from "next/navigation";
 import NavbarItemChip from "./navbarItemChip";
 import { Separator } from "@/components/ui/separator";
 import { HomeIcon } from "lucide-react";
+import SettingDialog from "../../settings/settingDialog";
 
 export default function DashboardNavbar() {
   const pathname = usePathname();
@@ -36,7 +31,7 @@ export default function DashboardNavbar() {
       name: "Team",
       href: "/dashboard/team/",
       svg: <Users className="size-5" />,
-    }
+    },
   ];
   return (
     <div className="flex h-screen flex-col py-[15px] pl-5">
@@ -56,22 +51,19 @@ export default function DashboardNavbar() {
       </div>
       <div className="flex flex-col gap-[16px]">
         <NavbarItemChip />
-        <Separator className="w-6 mx-auto"/>
+        <Separator className="w-6 mx-auto" />
         <TooltipProvider delayDuration={0}>
           <Tooltip>
-            <TooltipTrigger>
-              <Link href={"/"}>
+            <SettingDialog>
+              <TooltipTrigger>
                 <div
-                  className={`flex h-[40px] min-h-[40px] w-[45px] min-w-[45px] hover:text-primary text-secondary items-center justify-center rounded-[12px] border text-[20px] ${
-                    pathname === "/dashobard/settings"
-                      ? "bg-accent/75"
-                      : "border-transparent bg-background hover:bg-accent/50"
-                  }`}
+                  className={`flex h-[40px] min-h-[40px] border-transparent bg-background hover:bg-accent/50 w-[45px] min-w-[45px] hover:text-primary text-secondary items-center justify-center rounded-[12px] border text-[20px]`}
                 >
                   <Settings className="size-5" />
                 </div>
-              </Link>
-            </TooltipTrigger>
+              </TooltipTrigger>
+            </SettingDialog>
+
             <TooltipContent
               side="right"
               sideOffset={8}
---
+++ frontend/src/components/dashboard/settings/settingDialog.tsx
@@ -0,0 +1,105 @@
+"use client";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Separator } from "@/components/ui/separator";
+import { CreditCard, Home, Key, Stars, User } from "lucide-react";
+import { ReactNode, useState } from "react";
+import SettingGeneralPage from "./settingPages/general";
+import { Box } from "react-feather";
+import { CubeIcon } from "@radix-ui/react-icons";
+
+export default function SettingDialog(props: { children: ReactNode }) {
+  const [page, setPage] = useState("general");
+  const settings = [
+    {
+      name: "General",
+      icon: <Home className="size-4 text-blue-500" />,
+      value: "general",
+    },
+    {
+      name: "Profile",
+      icon: <User className="size-4 text-blue-500" />,
+      value: "profile",
+    },
+    {
+      name: "API",
+      icon: <Key className="size-4 text-blue-500" />,
+      value: "api",
+    },
+    {
+      name: "Metrics",
+      icon: <Box className="size-4 text-blue-500" />,
+      value: "metrics",
+    },
+    {
+      name: "Payment",
+      icon: <CreditCard className="size-4 text-blue-500" />,
+      value: "payment",
+    },
+    {
+      name: "My plan",
+      icon: <Stars className="size-4 text-blue-500" />,
+      value: "plan",
+    },
+  ];
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{props.children}</DialogTrigger>
+      <DialogContent className="flex flex-row gap-0 max-h-[70%] h-[70%] min-w-[80%] ring-1 ring-input !rounded-xl !shadow-none bg-transparent !p-0 overflow-hidden">
+        <Navbar page={page} setPage={setPage} settings={settings} />
+        <Content page={page} setPage={setPage} settings={settings} />
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+function Navbar(props: {
+  page: any;
+  setPage: (value: any) => void;
+  settings: Array<any>;
+}) {
+  return (
+    <div className="h-full bg-background/70 backdrop-blur-md min-w-[230px] border-r border-input pt-8 px-2">
+      <div className="text-secondary font-medium text-xs ml-4">Settings</div>
+      <div className="flex gap-2 flex-col mt-3">
+        {props.settings.map((item, i) => {
+          return (
+            <div
+              key={i}
+              className={`w-full px-4 py-[5px] flex flex-row gap-2 cursor-pointer select-none items-center hover:bg-zinc-400/15 text-primary font-medium text-sm rounded-[8px] ${
+                props.page === item.value ? "bg-zinc-400/15" : ""
+              }`}
+              onClick={() => props.setPage(item.value)}
+            >
+              {item.icon}
+              {item.name}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+function Content(props: {
+  page: any;
+  setPage: (value: any) => void;
+  settings: Array<any>;
+}) {
+  function content() {
+    switch (props.page) {
+      case "general":
+        return <SettingGeneralPage />;
+    }
+  }
+  return (
+    <div className="w-full h-full bg-white p-8 pl-10 flex flex-col gap-5">
+      <div className="text-2xl font-semibold">
+        {props.settings.map((setting, i) => {
+          return setting.value === props.page ? setting.name : "";
+        })}
+      </div>
+      <Separator />
+      {content()}
+    </div>
+  );
+}
