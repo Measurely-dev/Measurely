e7b9d17a88582ffe732958966f1ded9f4d60d638
zxk-afz <zakaryfofana@proton.me>
2024-12-09 14:29:48 -0500

    removed forced lastname to update setting

--- frontend/src/components/dashboard/setting-pages/general.tsx
+++ frontend/src/components/dashboard/setting-pages/general.tsx
@@ -130,7 +130,6 @@ export default function SettingGeneralPage() {
         btn_loading={loadingProfile}
         btn_disabled={
           firstName === '' ||
-          lastName === '' ||
           (firstName === user?.firstname && lastName === user?.lastname)
         }
         action={handleFirstLastNameSubmit}
