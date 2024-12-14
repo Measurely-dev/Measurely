662c1b5bdbce23ea6d827f67e1187448f67e904f
zxk-afz <zakaryfofana@proton.me>
2024-12-14 16:23:13 -0500

    fix: building error

--- frontend/src/app/dashboard/(loading)/(main)/wrapper.tsx
+++ frontend/src/app/dashboard/(loading)/(main)/wrapper.tsx
@@ -3,7 +3,6 @@
 import DashboardContent from '@/components/dashboard/content';
 import DashboardNavbar from '@/components/dashboard/navbar';
 import DashboardTopbar from '@/components/dashboard/topbar/topbar';
-import { ScrollArea } from '@/components/ui/scroll-area';
 export default function DashboardWrapper({ children }: { children: any }) {
   return (
     <div className='flex flex-row items-center gap-[40px] overflow-x-hidden pr-[5px]'>
