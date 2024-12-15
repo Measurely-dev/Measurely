ba8ed7a630b7956569ad632b572ecea3d5a2be40
zxk-afz <zakaryfofana@proton.me>
2024-12-14 20:05:19 -0500

    test: fix double scroll on chrome

--- frontend/src/app/dashboard/(loading)/(main)/wrapper.tsx
+++ frontend/src/app/dashboard/(loading)/(main)/wrapper.tsx
@@ -5,14 +5,18 @@ import DashboardNavbar from '@/components/dashboard/navbar';
 import DashboardTopbar from '@/components/dashboard/topbar/topbar';
 export default function DashboardWrapper({ children }: { children: any }) {
   return (
-    <div className='flex !max-h-screen flex-row items-center gap-[40px] overflow-x-hidden pr-[5px]'>
+    <div className='flex max-h-screen flex-row items-center gap-[40px] overflow-x-hidden pr-[5px]'>
       <div className='max-md:hidden'>
         <DashboardNavbar />
       </div>
-      <DashboardContent>
-        <DashboardTopbar />
-        <div className='flex flex-col'>{children}</div>
-      </DashboardContent>
+      <div className='w-full pl-[20px]'>
+        <DashboardContent>
+          <DashboardTopbar />
+          <div className='flex flex-col'>
+            {children}
+          </div>
+        </DashboardContent>
+      </div>
     </div>
   );
 }
