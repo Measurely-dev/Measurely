e94e0100fff132a2b92f798516e4c26ca0513425
zxk-afz <zakaryfofana@proton.me>
2024-12-24 19:52:36 -0500

    fea: landing page showcase

--- frontend/src/components/website/sections/showcase.tsx
+++ frontend/src/components/website/sections/showcase.tsx
@@ -2,14 +2,12 @@ import Link from 'next/link';
 import WebButton from '../button';
 import WebChip from '../chip';
 import Card1 from '../showcase/card-1';
-import Card2 from '../showcase/card-2';
-import Card3 from '../showcase/card-3';
-import Card4 from '../showcase/card-4';
 import Card5 from '../showcase/card-5';
-
+import Image from 'next/image';
+import Preview from '../../../../public/preview.png';
 export default function ShowcaseLandingSection() {
   return (
-    <div className='relative flex h-screen min-h-[780px] w-screen flex-col items-center pt-[30vh] max-lg:pt-[20vh] max-md:pt-[23vh]'>
+    <div className='relative flex h-screen min-h-[780px] w-screen flex-col items-center pt-[22vh] max-lg:pt-[20vh] max-md:pt-[15vh]'>
       <WebChip color='default' href='/docs/getting-started/introduction'>
         <span className='mr-1 font-medium'>Measurely</span> is the new way
       </WebChip>
@@ -23,13 +21,21 @@ export default function ShowcaseLandingSection() {
       <Link href='/register'>
         <WebButton className='mt-8'>Get started</WebButton>
       </Link>
+      <div className='relative mx-2 mt-12 max-lg:mt-10 h-full w-fit max-w-[1100px] p-1 pb-0 max-sm:ml-40 max-sm:w-[120vw] max-sm:min-w-[450px] max-sm:max-w-[120vw]'>
+        <Image
+          src={Preview}
+          alt='Preview image'
+          className='relative z-10 h-full w-fit rounded-xl rounded-b-none border-[10px] border-background'
+        />
+        <div className='absolute left-0 top-0 h-full w-full animate-gradient rounded-2xl rounded-b-none bg-gradient-to-r from-gray-400 via-gray-300 to-gray-200' />
+      </div>
       <Card5 className='absolute -left-16 -top-8 rotate-[9deg] max-lg:hidden' />
-      <Card1 className='absolute -right-16 -top-8 rotate-[-9deg] max-lg:hidden' />
-      <div className='absolute bottom-0 flex h-10 w-full max-w-[1200px] items-center justify-center'>
+      <Card1 className='absolute -right-16 -top-10 rotate-[-9deg] max-lg:hidden' />
+      {/* <div className='absolute bottom-0 flex h-10 w-full max-w-[1200px] items-center justify-center'>
         <Card2 className='absolute -bottom-10 right-16 rotate-[7deg] max-xl:w-[300px] max-lg:translate-x-[-45%] max-lg:rotate-0 max-md:hidden' />
         <Card3 className='absolute bottom-[90px] max-xl:w-[320px] max-lg:hidden' />
         <Card4 className='absolute -bottom-10 left-16 rotate-[-7deg] max-xl:w-[300px] max-lg:right-[50%] max-lg:translate-x-[50%] max-lg:rotate-0 max-md:left-[50%] max-md:w-[350px] max-md:translate-x-[-50%]' />
-      </div>
+      </div> */}
     </div>
   );
 }
--- frontend/src/components/website/showcase/card-1.tsx
+++ frontend/src/components/website/showcase/card-1.tsx
@@ -1,62 +1,37 @@
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { MinusIcon } from 'lucide-react';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
+import { CheckIcon } from 'lucide-react';
 
 export default function Card1(props: { className?: string }) {
   return (
-    <Card className={`w-[340px] rounded-2xl p-5 shadow-sm ${props.className}`}>
-      <div className='absolute bottom-[-10px] left-[-10px] z-10 flex size-8 items-center justify-center rounded-[10px] border bg-zinc-300/30 font-mono text-sm text-red-400 backdrop-blur-md'>
-        -1
+    <Card className={`w-[320px] rounded-2xl p-0 shadow-sm ${props.className}`}>
+      <div className='absolute bottom-[-10px] left-[-10px] z-10 flex size-8 items-center justify-center rounded-[10px] border bg-zinc-300/30 font-mono text-sm text-blue-400 backdrop-blur-md'>
+        +1
       </div>
-      <div className='absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center gap-3 rounded-2xl bg-zinc-300/15 text-sm text-red-400 backdrop-blur-md'>
-        <div className='flex aspect-square h-[60px] items-center justify-center rounded-full border border-red-400 bg-red-400/10 text-red-400'>
-          <MinusIcon className='size-8 stroke-[2]' />
+      <div className='absolute flex h-full w-full flex-col items-center justify-center gap-3 rounded-2xl bg-zinc-300/15 text-sm text-blue-400 backdrop-blur-md'>
+        <div className='flex aspect-square h-[60px] items-center justify-center rounded-full border border-blue-400 bg-blue-400/10 text-blue-400'>
+          <CheckIcon className='size-8 !stroke-[1]' />
         </div>
-        Metric deleted
+        Metric created
       </div>
-      <CardHeader className='p-0'>
-        <CardTitle>New milestone</CardTitle>
-        <CardDescription className='text-xs'>
-          By creating a milestone, you send a step request to the client of this
-          project.
-        </CardDescription>
+      <CardHeader className='flex flex-row items-center p-5'>
+        <div className='flex w-full flex-col gap-1'>
+          <span className='text-xs text-secondary'>
+            • Created March 30, 2024
+          </span>
+          <span className='text-xl font-semibold'>New users</span>
+        </div>
+        <div className='flex aspect-square h-[40px] items-center justify-center rounded-full border border-green-400 bg-green-400/10 text-green-400'>
+          <CheckIcon className='size-6' />
+        </div>
       </CardHeader>
-      <CardContent className='mt-5 flex w-full flex-col gap-3 p-0'>
-        <div className='grid w-full items-center gap-1'>
-          <Label htmlFor='Title' className='text-xs'>
-            Title
-          </Label>
-          <Input
-            type='text'
-            placeholder='Title'
-            className='text-xs placeholder:text-xs'
-          />
-        </div>
-        <div className='grid w-full items-center gap-1'>
-          <Label htmlFor='Title' className='text-xs'>
-            Description
-          </Label>
-          <Input
-            type='text'
-            placeholder='Descripiton'
-            className='text-xs placeholder:text-xs'
-          />
-        </div>
-        <Button
-          className='w-full bg-red-500/20 text-red-500 hover:bg-red-500/30 hover:text-red-500'
-          variant={'destructiveOutline'}
-          size='sm'
-        >
-          Create milestone
-        </Button>
+      <Separator className='w-full' />
+      <CardContent className='flex flex-col p-5'>
+        <span className='text-sm'>Metric description</span>
+        <span className='mt-1 text-xs text-secondary'>
+          Introducing our cutting-edge Client Portal Software, a revolutionary
+          SaaS application designed specifically for modern Design and Creative
+        </span>
       </CardContent>
     </Card>
   );
