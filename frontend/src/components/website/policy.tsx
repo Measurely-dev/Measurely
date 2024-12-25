cc6470fb20e3028755be5f76fbb496b977b88f3e
zxk-afz <zakaryfofana@proton.me>
2024-12-25 01:23:58 -0500

    fix: policies markdown css

--- frontend/src/components/website/policy.tsx
+++ frontend/src/components/website/policy.tsx
@@ -7,8 +7,8 @@ export default function PolicyWrapper(props: {
   return (
     <div className='flex min-h-screen flex-col items-center'>
       <div className='flex w-full flex-col gap-4 text-primary'>
-        <h1 className='mb-12'>{props.title}</h1>
-        <div className='text-lg text-secondary'>
+        <h1 className='text-4xl'>{props.title}</h1>
+        <div className='text-lg text-muted-foreground'>
           Effective date: {props.updatedDate}
         </div>
         <h2 className='mb-2'>Summary</h2>
