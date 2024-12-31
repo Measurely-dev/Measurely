534c485d76ad1ebe7a2175165ca4a38df780a171
zxk-afz <zakaryfofana@proton.me>
2024-12-31 13:27:48 -0500

    fea: added sitemap.xml and robot.txt

---
+++ frontend/next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://www.measurely.dev',
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+  };
+  
+  module.exports = config;
+  
