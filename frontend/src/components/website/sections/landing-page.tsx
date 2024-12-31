50b308816e1259e28383622a5be581e1f8bc9eab
zxk-afz <zakaryfofana@proton.me>
2024-12-31 14:47:45 -0500

    fea: changed the landing page image size

--- frontend/src/components/website/sections/landing-page.tsx
+++ frontend/src/components/website/sections/landing-page.tsx
@@ -29,11 +29,11 @@ export default function LandingSection() {
       </h1>
       <Link href='/register'>
         <WebButton className='mt-10 px-3.5 py-2'>
-          <div className='text-lg'>Get started</div>
+          <div className='text-base'>Get started</div>
         </WebButton>
       </Link>
       <div className='absolute left-[0] top-[0] z-[-10] h-[100%] w-screen bg-accent/40 backdrop-blur-2xl' />
-      <div className='relative mx-2 mt-20 flex h-full w-fit max-w-[90%] select-none items-center justify-center max-lg:mt-10'>
+      <div className='relative mx-2 mt-20 flex h-full w-fit max-w-[90%] select-none items-bottom justify-bottom max-lg:mt-10'>
         <motion.div
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
@@ -42,7 +42,7 @@ export default function LandingSection() {
           className='absolute top-2 z-[-11] h-[80%] w-[100%] animate-gradient bg-gradient-to-r from-purple-500 via-blue-500 to-pink-400 max-sm:h-[100%]'
         />
         <motion.div
-          className='h-full w-full'
+          className='h-full w-full flex max-md:items-end'
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           viewport={{ once: true }}
@@ -51,9 +51,9 @@ export default function LandingSection() {
           <Image
             src={Preview}
             alt='Preview image'
-            height={2108}
-            width={1820}
-            className='relative z-10 mx-auto h-full w-fit rounded-xl rounded-b-none bg-background object-contain p-2 px-1 pb-0 ring-4 ring-background/50 max-sm:ml-5 max-sm:w-[125vw] max-sm:min-w-[450px] max-sm:max-w-[125vw]'
+            height={2762}
+            width={1762}
+            className='relative z-10 mx-auto h-full max-md:h-fit w-fit rounded-2xl rounded-b-none bg-background object-contain p-2 px-1 pb-0 ring-4 ring-background/50 max-sm:ml-5 max-sm:w-[125vw] max-sm:min-w-[700px] '
             priority
           />
         </motion.div>
