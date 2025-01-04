b30673d3ced0d2777b84c84e712087609c55ba9b
zxk-afz <zakaryfofana@proton.me>
2025-01-03 21:05:19 -0500

    feat: block toolbar

--- frontend/src/app/dashboard/(loading)/(main)/page.tsx
+++ frontend/src/app/dashboard/(loading)/(main)/page.tsx
@@ -1,7 +1,5 @@
 'use client';
-import { ChartsCard } from '@/components/dashboard/charts-card';
 import DashboardContentContainer from '@/components/dashboard/container';
-import UpgradeCard from '@/components/dashboard/upgrade-card';
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -10,9 +8,25 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+import { Plus, Rocket, Sparkle } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { useContext } from 'react';
+import { ProjectsContext, UserContext } from '@/dash-context';
+import PlansDialog from '@/components/dashboard/plans-dialog';
+import MetricStats from '@/components/dashboard/metric-stats';
+import { useRouter } from 'next/navigation';
+import { MetricType } from '@/types';
 
 export default function DashboardHomePage() {
+  const { projects, activeProject, setProjects } = useContext(ProjectsContext);
+  const [activeMetric, setActiveMetric] = useState(0);
+  const router = useRouter();
+  const [chartData, setChartData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     document.title = 'Dashboard | Measurely';
     const metaDescription = document.querySelector('meta[name="description"]');
@@ -23,6 +37,19 @@ export default function DashboardHomePage() {
       );
     }
   }, []);
+
+  useEffect(() => {
+    const new_index =
+      projects[activeProject].metrics === null
+        ? 0
+        : projects[activeProject].metrics.length === 0
+          ? 0
+          : projects[activeProject].metrics.length - 1;
+
+    if (activeMetric > new_index) {
+      setActiveMetric(new_index);
+    }
+  }, [activeProject]);
   return (
     <DashboardContentContainer className='mt-0 flex w-full pt-[15px]'>
       <Breadcrumb>
@@ -39,7 +66,106 @@ export default function DashboardHomePage() {
         </BreadcrumbList>
       </Breadcrumb>
       <UpgradeCard />
-      <ChartsCard />
+      <MetricStats
+        className='rounded-b-[12px] p-5'
+        stats={[
+          {
+            title: 'Metric used',
+            description: 'Across this project',
+            value: projects[activeProject].metrics?.length,
+          },
+          {
+            title: 'Number of dual metric',
+            description: 'Across this project',
+            value: projects[activeProject].metrics?.filter(
+              (m) => m.type === MetricType.Dual,
+            ).length,
+          },
+          {
+            title: 'Number of basic metric',
+            description: 'Across this project',
+            value: projects[activeProject].metrics?.filter(
+              (m) => m.type === MetricType.Base,
+            ).length,
+          },
+          {
+            title: 'Team members',
+            description: 'Coming soon',
+            value: 'N/A',
+          },
+        ]}
+      />
+      <Toolbar />
     </DashboardContentContainer>
+  );
+}
+
+function UpgradeCard() {
+  const { user } = useContext(UserContext);
+  function render() {
+    switch (user?.plan.identifier) {
+      case 'starter':
+        return (
+          <Card className='mt-5 rounded-3xl rounded-b-none border-none bg-black'>
+            <CardContent className='flex flex-row items-center justify-between gap-5 p-5 max-md:flex-col'>
+              <div className='flex flex-col max-md:w-full'>
+                <div className='flex flex-row items-center gap-3'>
+                  <Rocket className='size-5 text-white' />
+                  <div className='text-md font-semibold text-white'>
+                    You're using the {user?.plan.name} plan
+                  </div>
+                </div>
+                <div className='text-sm text-white/70'>
+                  You can unlock your limits by upgrading to the next plan.
+                </div>
+              </div>
+              <PlansDialog>
+                <Button
+                  className='rounded-[12px] !bg-background !text-primary hover:opacity-80 max-md:w-full'
+                  variant={'default'}
+                >
+                  Upgrade
+                </Button>
+              </PlansDialog>
+            </CardContent>
+          </Card>
+        );
+      default:
+        return (
+          <Card className='mt-5 animate-gradient rounded-[12px] rounded-b-none border-none bg-background bg-gradient-to-r from-purple-500 via-blue-500 to-pink-500'>
+            <CardContent className='flex flex-row items-center justify-between gap-5 p-5 max-md:flex-col'>
+              <div className='flex flex-col max-md:w-full'>
+                <div className='flex flex-row items-center gap-3'>
+                  <div className='flex flex-row items-center gap-1 rounded-full bg-accent p-1 px-2 text-xs font-medium'>
+                    <Sparkle className='size-3' />
+                    {user?.plan.name}
+                  </div>
+                  <div className='text-md font-semibold text-white'>
+                    Welcome back,{' '}
+                    <span className='font-semibold capitalize'>
+                      {user?.firstname}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        );
+    }
+  }
+  return render();
+}
+
+function Toolbar() {
+  return (
+    <div className='mt-5 flex w-full items-center justify-between rounded-s-[12px] rounded-e-3xl bg-accent p-1 pl-4'>
+      <div className='font-medium'>Blocks</div>
+      <div className='flex items-center gap-2'>
+        <Button className='rounded-2xl rounded-s-[12px]'>
+          <Plus className='mr-2 size-4' />
+          Create new block
+        </Button>
+      </div>
+    </div>
   );
 }
