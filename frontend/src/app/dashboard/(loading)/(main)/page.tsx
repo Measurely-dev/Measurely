4f604576f71207ae58e11fccaaa1bd213c9e75e6
zxk-afz <zakaryfofana@proton.me>
2025-01-04 19:36:49 -0500

    fea: metric dialog UI

--- frontend/src/app/dashboard/(loading)/(main)/page.tsx
+++ frontend/src/app/dashboard/(loading)/(main)/page.tsx
@@ -8,24 +8,51 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
-import { useEffect, useState } from 'react';
-
-import { Plus, Rocket, Sparkle } from 'lucide-react';
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useEffect,
+  useState,
+} from 'react';
+import { Sortable, SortableItem } from '@/components/ui/sortable';
+import { Plus, Rocket, Sparkle, X } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { useContext } from 'react';
 import { ProjectsContext, UserContext } from '@/dash-context';
 import PlansDialog from '@/components/dashboard/plans-dialog';
 import MetricStats from '@/components/dashboard/metric-stats';
 import { useRouter } from 'next/navigation';
 import { MetricType } from '@/types';
+import { closestCorners } from '@dnd-kit/core';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { AreaChart } from '@/components/ui/area-chart';
+import {
+  AreaChartData,
+  BarChartData,
+  BarListData,
+  ComboChartData,
+  DonutChartData,
+} from '@/components/global/block-fake-data';
+import { DonutChart } from '@/components/ui/donut-chart';
+// import { BarList } from '@/components/ui/bar-list';
+import { ComboChart } from '@/components/ui/combo-chart';
+import { BarChart } from '@/components/ui/bar-chart';
+import { BarList } from '@/components/ui/bar-list';
 
 export default function DashboardHomePage() {
   const { projects, activeProject, setProjects } = useContext(ProjectsContext);
   const [activeMetric, setActiveMetric] = useState(0);
   const router = useRouter();
-  const [chartData, setChartData] = useState<any[]>([]);
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     document.title = 'Dashboard | Measurely';
@@ -67,7 +94,7 @@ export default function DashboardHomePage() {
       </Breadcrumb>
       <UpgradeCard />
       <MetricStats
-        className='rounded-b-[12px] p-5'
+        className='p-5'
         stats={[
           {
             title: 'Metric used',
@@ -159,18 +186,273 @@ function UpgradeCard() {
 
 function Toolbar() {
   return (
-    <div className='mt-5 flex w-full items-center justify-between rounded-e-3xl rounded-s-[12px] bg-accent p-1 pl-4'>
+    <div className='mt-0 flex w-full items-center justify-between rounded-[12px] rounded-t-none border-t bg-accent p-5 py-2'>
       <div className='font-medium'>Blocks</div>
       <div className='flex items-center gap-2'>
-        <Button className='rounded-2xl rounded-s-[12px]'>
-          <Plus className='mr-2 size-4' />
-          Create new block
-        </Button>
+        <BlocksDialog>
+          <Button className='rounded-[12px]'>
+            <Plus className='mr-2 size-4' />
+            Create new block
+          </Button>
+        </BlocksDialog>
       </div>
     </div>
   );
 }
 
+const dataConfig = {
+  speicalTricks: [
+    {
+      id: 1,
+      name: 'Indy Backflip',
+      colSpan: 2,
+    },
+    {
+      id: 2,
+      name: 'Pizza Guy',
+      colSpan: 1,
+    },
+    {
+      id: 3,
+      name: '360 Varial McTwist',
+      colSpan: 1,
+    },
+    {
+      id: 4,
+      name: 'Kickflip Backflip',
+      colSpan: 2,
+    },
+  ],
+};
 function Blocks() {
-  return <div></div>;
+  const [specialTricks, setSpecialTricks] = useState(dataConfig.speicalTricks);
+  return (
+    <div className='mt-5 pb-20'>
+      <Sortable
+        orientation='mixed'
+        collisionDetection={closestCorners}
+        value={specialTricks}
+        onValueChange={setSpecialTricks}
+        overlay={<div className='size-full rounded-[12px] bg-primary/10' />}
+      >
+        <div className='grid grid-cols-2 gap-4'>
+          {specialTricks.map((item) => (
+            <SortableItem key={item.id} value={item.id} asTrigger asChild>
+              <Card
+                className='flex h-[40vh] w-full items-center justify-center rounded-[12px] bg-accent hover:bg-accent/80'
+                style={
+                  item.colSpan === 2
+                    ? { gridColumn: 'span 2 / span 2' }
+                    : { gridColumn: 'span 1 / span 1' }
+                }
+              >
+                <CardHeader className='items-center'>
+                  <CardTitle>{item.name}</CardTitle>
+                </CardHeader>
+              </Card>
+            </SortableItem>
+          ))}
+        </div>
+      </Sortable>
+    </div>
+  );
+}
+
+function BlocksDialog(props: { children: ReactNode }) {
+  const [value, setValue] = useState<number>(1);
+
+  const blockType = [
+    {
+      category: 'Wide Blocks',
+      blocks: [
+        {
+          name: 'Area Chart',
+          value: 1,
+          description:
+            'Visualizes data trends over time with shaded areas, highlighting volume or changes.',
+          chart: (
+            <AreaChart
+              className='h-40'
+              data={AreaChartData}
+              index='date'
+              categories={['SolarPanels', 'Inverters']}
+              valueFormatter={(number: number) =>
+                `$${Intl.NumberFormat('us').format(number).toString()}`
+              }
+              showYAxis={false}
+            />
+          ),
+        },
+        {
+          name: 'Bar Chart',
+          value: 2,
+          description:
+            'Represents data in a horizontal bar format, best for ranking and side-by-side comparisons.',
+          chart: (
+            <BarChart
+              className='h-40'
+              data={BarChartData}
+              index='date'
+              categories={['SolarPanels', 'Inverters']}
+              valueFormatter={(number: number) =>
+                `$${Intl.NumberFormat('us').format(number).toString()}`
+              }
+              showYAxis={false}
+            />
+          ),
+        },
+        {
+          name: 'Combo Chart',
+          value: 3,
+          description:
+            'Combines a bar chart and line chart in one, great for comparing totals and trends simultaneously.',
+          chart: (
+            <ComboChart
+              className='h-40'
+              data={ComboChartData}
+              index='date'
+              enableBiaxial={true}
+              barSeries={{
+                categories: ['SolarPanels'],
+                showYAxis: false,
+              }}
+              lineSeries={{
+                categories: ['Inverters'],
+                showYAxis: false,
+                colors: ['red'],
+              }}
+            />
+          ),
+        },
+        {
+          name: 'Bar List',
+          value: 4,
+          description:
+            'Displays data in a vertical bar chart format, ideal for comparing multiple categories.',
+          chart: <BarList data={BarListData} />,
+        },
+      ],
+    },
+    {
+      category: 'Compact Blocks',
+      blocks: [
+        {
+          name: 'Donut Chart',
+          value: 6,
+          description:
+            'A variation of the pie chart with a central hole, useful for emphasizing data segments.',
+          chart: (
+            <DonutChart
+              className='h-40'
+              data={DonutChartData}
+              category='name'
+              value='amount'
+              valueFormatter={(number: number) =>
+                `$${Intl.NumberFormat('us').format(number).toString()}`
+              }
+            />
+          ),
+        },
+        {
+          name: 'Pie Chart View',
+          value: 5,
+          description:
+            'Shows proportions of a whole using a pie chart, perfect for visualizing percentages or ratios.',
+          chart: (
+            <DonutChart
+              className='h-40'
+              data={DonutChartData}
+              category='name'
+              value='amount'
+              variant='pie'
+              valueFormatter={(number: number) =>
+                `$${Intl.NumberFormat('us').format(number).toString()}`
+              }
+            />
+          ),
+        },
+      ],
+    },
+  ];
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{props.children}</DialogTrigger>
+      <DialogContent className='h-[80vh] max-h-[600px] w-[95%] max-w-[700px] p-0 max-sm:w-[100%]'>
+        <DialogHeader className='px-5 pt-5 max-sm:text-start'>
+          <DialogTitle>Select Block</DialogTitle>
+          <DialogDescription>
+            Custom components to showcase or compare metric data on your
+            overview page.
+          </DialogDescription>
+          <DialogClose className='absolute right-5 top-3 max-sm:hidden'>
+            <Button
+              type='button'
+              size={'icon'}
+              variant='secondary'
+              className='rounded-[12px]'
+            >
+              <X />
+            </Button>
+          </DialogClose>
+        </DialogHeader>
+        <div className='flex flex-col gap-5 overflow-y-auto border-t px-5 pb-10'>
+          {blockType.map((blockSection, i) => (
+            <div key={i}>
+              <div className='font-medium text-base capitalize text-primary mt-5 mb-2'>{blockSection.category}</div>
+              <div
+                className={`grid grid-cols-1 gap-5 ${blockSection.category === 'Compact Blocks' ? 'grid-cols-2' : 'grid-cols-1'}`}
+              >
+                {blockSection.blocks.map((blockItem, j) => (
+                  <BlockItem
+                    key={j}
+                    description={blockItem.description}
+                    name={blockItem.name}
+                    value={blockItem.value}
+                    state={value}
+                    setState={setValue}
+                    chart={blockItem.chart}
+                  />
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+function BlockItem(props: {
+  name: string;
+  value: number;
+  description: string;
+  chart?: ReactNode;
+  state: any;
+  setState: Dispatch<SetStateAction<number>>;
+}) {
+  return (
+    <div
+      className={`flex w-full select-none flex-col gap-1 rounded-xl border p-3 transition-all duration-150 ${
+        props.state === props.value
+          ? 'cursor-pointer bg-blue-500/5 ring-2 ring-blue-500'
+          : 'cursor-pointer hover:bg-accent/50'
+      }`}
+      onClick={() => {
+        props.setState(props.value);
+      }}
+    >
+      <div className='text-sm font-medium'>{props.name}</div>
+      <div className='text-xs font-light text-secondary'>
+        {props.description}
+      </div>
+      {props.value === 4 ? (
+        <BarList className='mt-4' data={BarListData} />
+      ) : (
+        <div className='pointer-events-none mx-auto mt-4 flex w-full select-none items-center justify-center'>
+          {props.chart}
+        </div>
+      )}
+    </div>
+  );
 }
---
+++ frontend/src/components/global/block-fake-data.tsx
@@ -0,0 +1,62 @@
+export const AreaChartData = [
+  { date: 'Jan 23', SolarPanels: 2890, Inverters: 2338 },
+  { date: 'Feb 23', SolarPanels: 2756, Inverters: 2103 },
+  { date: 'Mar 23', SolarPanels: 3322, Inverters: 2194 },
+  { date: 'Apr 23', SolarPanels: 3470, Inverters: 2108 },
+  { date: 'May 23', SolarPanels: 3475, Inverters: 1812 },
+  { date: 'Jun 23', SolarPanels: 3129, Inverters: 1726 },
+  { date: 'Jul 23', SolarPanels: 3490, Inverters: 1982 },
+  { date: 'Aug 23', SolarPanels: 2903, Inverters: 2012 },
+  { date: 'Sep 23', SolarPanels: 2643, Inverters: 2342 },
+  { date: 'Oct 23', SolarPanels: 2837, Inverters: 2473 },
+  { date: 'Nov 23', SolarPanels: 2954, Inverters: 3848 },
+  { date: 'Dec 23', SolarPanels: 3239, Inverters: 3736 },
+];
+
+export const DonutChartData = [
+  { name: 'SolarCells', amount: 4890 },
+  { name: 'Glass', amount: 2103 },
+  { name: 'JunctionBox', amount: 2050 },
+  { name: 'Adhesive', amount: 1300 },
+  { name: 'BackSheet', amount: 1100 },
+  { name: 'Frame', amount: 700 },
+  { name: 'Encapsulant', amount: 200 },
+];
+
+export const BarListData = [
+  { name: '/home', value: 843 },
+  { name: '/imprint', value: 46 },
+  { name: '/cancellation', value: 3 },
+  { name: '/blocks', value: 108 },
+  { name: '/documentation', value: 384 },
+];
+
+export const ComboChartData = [
+  { date: 'Jan 23', SolarPanels: 2890, Inverters: 2338 },
+  { date: 'Feb 23', SolarPanels: 2756, Inverters: 2103 },
+  { date: 'Mar 23', SolarPanels: 3322, Inverters: 2194 },
+  { date: 'Apr 23', SolarPanels: 3470, Inverters: 2108 },
+  { date: 'May 23', SolarPanels: 3475, Inverters: 1812 },
+  { date: 'Jun 23', SolarPanels: 3129, Inverters: 1726 },
+  { date: 'Jul 23', SolarPanels: 3490, Inverters: 1982 },
+  { date: 'Aug 23', SolarPanels: 2903, Inverters: 2012 },
+  { date: 'Sep 23', SolarPanels: 2643, Inverters: 2342 },
+  { date: 'Oct 23', SolarPanels: 2837, Inverters: 2473 },
+  { date: 'Nov 23', SolarPanels: 2954, Inverters: 3848 },
+  { date: 'Dec 23', SolarPanels: 3239, Inverters: 3736 },
+];
+
+export const BarChartData = [
+  { date: 'Jan 23', SolarPanels: 2890, Inverters: 2338 },
+  { date: 'Feb 23', SolarPanels: 2756, Inverters: 2103 },
+  { date: 'Mar 23', SolarPanels: 3322, Inverters: 2194 },
+  { date: 'Apr 23', SolarPanels: 3470, Inverters: 2108 },
+  { date: 'May 23', SolarPanels: 3475, Inverters: 1812 },
+  { date: 'Jun 23', SolarPanels: 3129, Inverters: 1726 },
+  { date: 'Jul 23', SolarPanels: 3490, Inverters: 1982 },
+  { date: 'Aug 23', SolarPanels: 2903, Inverters: 2012 },
+  { date: 'Sep 23', SolarPanels: 2643, Inverters: 2342 },
+  { date: 'Oct 23', SolarPanels: 2837, Inverters: 2473 },
+  { date: 'Nov 23', SolarPanels: 2954, Inverters: 3848 },
+  { date: 'Dec 23', SolarPanels: 3239, Inverters: 3736 },
+];
