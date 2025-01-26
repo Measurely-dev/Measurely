4888b8e54e99c53f43f0751b969998ccd2ff8c80
zxk-afz <zakaryfofana@proton.me>
2025-01-25 22:02:17 -0500

    refactor: update sitemap and robots configuration, remove obsolete files

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -3,6 +3,10 @@ const config = {
   siteUrl: 'https://www.measurely.dev',
   generateRobotsTxt: true,
   sitemapSize: 5000,
+  generateIndexSitemap: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  autoLastmod: true,
 };
 
 module.exports = config;
