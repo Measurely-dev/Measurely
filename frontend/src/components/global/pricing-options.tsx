a71acf6f4294906f3cb373e1066240564a4bd0d1
zxk-afz <zakaryfofana@proton.me>
2025-01-29 15:11:18 -0500

    fea : enhance pricing options component with badge for discount display and adjust layout for consistency

--- frontend/src/components/global/pricing-options.tsx
+++ frontend/src/components/global/pricing-options.tsx
@@ -3,14 +3,15 @@ import { getEventAmount } from '@/utils';
 import { Slider } from '../ui/slider';
 import { ToggleGroup, ToggleGroupItem } from '../ui/toggle-group';
 import { Dispatch, SetStateAction } from 'react';
+import { Badge } from '../ui/badge';
 
 // Interface defining props for the PricingOptions component
 interface PricingOptionsProps {
-  sliderValue: number[];            // Current value of slider
-  setSliderValue: (value: number[]) => void;  // Function to update slider value
+  sliderValue: number[]; // Current value of slider
+  setSliderValue: (value: number[]) => void; // Function to update slider value
   billingPeriod: 'month' | 'year'; // Current billing period selection
   setBillingPeriod: Dispatch<SetStateAction<'month' | 'year'>>; // Function to update billing period
-  type?: 'page' | 'dialog';        // Display type of the component
+  type?: 'page' | 'dialog'; // Display type of the component
 }
 
 // Component for displaying and managing pricing options
@@ -31,7 +32,7 @@ export const PricingOptions = ({
     >
       {/* Event quantity slider section */}
       <div
-        className={`flex w-full max-w-[400px] flex-row items-center gap-4 ${type === 'dialog' ? 'mt-10' : 'mx-auto mt-[40px] mb-10'}`}
+        className={`flex w-full max-w-[400px] flex-row items-center gap-4 ${type === 'dialog' ? 'mt-10' : 'mx-auto mb-10 mt-[40px]'}`}
       >
         <span className='font-mono text-sm font-medium text-primary'>10K</span>
         <Slider
@@ -76,9 +77,11 @@ export const PricingOptions = ({
           className='flex items-center rounded-[10px] data-[state=on]:!bg-background'
         >
           Yearly
-          <span className='bg-gradient-to-r from-purple-500 via-blue-500 to-pink-400 bg-clip-text font-mono text-transparent'>
-            -20%
-          </span>
+          <Badge className='pointer-events-none rounded-[8px] border bg-background shadow-none'>
+            <div className='bg-gradient-to-r from-purple-500 via-blue-500 to-pink-400 bg-clip-text font-mono text-transparent'>
+              -20%
+            </div>
+          </Badge>
         </ToggleGroupItem>
       </ToggleGroup>
     </div>
