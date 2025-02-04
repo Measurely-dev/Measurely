8cbdfebf6044f5e7b543ead6ec0395ca7d7d5baf
zxk-afz <zakaryfofana@proton.me>
2025-02-03 19:39:20 -0500

    refactor: remove unused SVG files and update metadata for improved SEO

--- content/app/layout.tsx
+++ content/app/layout.tsx
@@ -21,10 +21,21 @@ export const metadata: Metadata = {
     template: "%s | Measurely",
   },
   openGraph: {
-    url: "./",
-    siteName: "Measurely",
-    locale: "en_US",
     type: "website",
+    locale: "en_US",
+    url: "https://content.measurely.dev",
+    siteName: "Measurely",
+    title: "Measurely - Track your metrics effectively",
+    description:
+      "Track and analyze your metrics seamlessly with Measurely. Perfect for developers and data-driven teams.",
+    images: [
+      {
+        url: "https://media.measurely.dev/opengraph.png",
+        width: 1200,
+        height: 630,
+        alt: "Measurely Dashboard Preview",
+      },
+    ],
   },
   other: {
     "msapplication-TileColor": "#fff",
@@ -48,19 +59,6 @@ const navbar = (
     }
     projectLink="https://github.com/measurely-dev/measurely"
     projectIcon={<GitHubIcon className="size-6" />}
-    chatLink="https://twitter.com/getmeasurely"
-    chatIcon={
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        x="0px"
-        y="0px"
-        width="24"
-        height="24"
-        viewBox="0 0 50 50"
-      >
-        <path d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z"></path>
-      </svg>
-    }
   />
 );
 const footer = (
@@ -81,9 +79,10 @@ export default async function RootLayout({
         <Layout
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/Measurely-dev/Measurely/tree/main/docs"
+          docsRepositoryBase="https://github.com/Measurely-dev/Measurely/tree/main/content"
           footer={footer}
           darkMode={false}
+          nextThemes={{ defaultTheme: "light" }}
         >
           {children}
         </Layout>
